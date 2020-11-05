1a8ba2215e209109b299cc0b4d5c1bf56cfc9714
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2020-11-05 15:54:39 +0100

    Allow filtering of human or mouse windows

--- runbatch_metagen_window_filter.sh
+++ runbatch_metagen_window_filter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Colin Davenport, April 2020
+# Colin Davenport, April-Nov 2020
 
 # Operates on window output files from runbatch_sambamba_depth.sh
 # sorted descending in col5 for within experiment clarity and to find regions with excessive counts 
@@ -7,10 +7,34 @@
 
 for i in `ls *window.txt`
         do
-	# filter out human reads
-	grep -v "1_1_1"  "$i" > "$i".filt.csv
 
-	# filter out human reads and sort by total reads per window
-	grep -v "1_1_1"  "$i" | sort -t$'\t' -k5 -nr > "$i".filt.sort.csv
+	# Change following line if needed to human or mouse
+	species=human
+
+	if [ $species == "human" ]
+		then
+
+		echo "Filtering windows, excluding" $species
+
+		# filter out human reads
+		grep -v "1_1_1"  "$i" > "$i".filt.csv
+
+		# filter out human reads and sort by total reads per window
+		grep -v "1_1_1"  "$i" | sort -t$'\t' -k5 -nr > "$i".filt.sort.csv
+
+
+	fi
+	if [ $species == "mouse" ]
+		then
+
+		echo "Filtering windows, excluding" $species
+
+		# filter out mouse reads
+        	grep -v "^chr"  "$i" > "$i".filt.csv
+
+		# filter out mouse reads and sort by total reads per window
+        	grep -v "^chr"  "$i" | sort -t$'\t' -k5 -nr > "$i".filt.sort.csv
+	fi
+
 done
 
