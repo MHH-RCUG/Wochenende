3e52ab34d006e7f755af2cb0beccc829e30d9d16
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2020-12-15 16:42:40 +0100

    Add wait to batch reporting

--- reporting/runbatch_Wochenende_reporting.sh
+++ reporting/runbatch_Wochenende_reporting.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 ## Detects all bam.txt files in the current folder and runs the reporting SLURM script
 
+echo "INFO: Starting batch reporting"
 
 for bamtxt in `ls *.bam.txt`
         do
         # run local
         bash run_Wochenende_reporting_SLURM.sh $bamtxt &
 done
+
+echo "INFO: Waiting for SLURM scripts to complete"
+wait
+
+echo "INFO: Completed batch reporting"
+
