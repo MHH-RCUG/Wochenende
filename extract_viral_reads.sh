49bdca42c55b8896a47ff017013e87f782ea2e55
Colin Davenport <davenport.colin@mh-hannover.de>
2021-03-23 13:27:38 +0100

    First working version of extract viral reads for replotting

--- extract_viral_reads.sh
+++ extract_viral_reads.sh
@@ -5,9 +5,18 @@
 # Use script get_wochenende.sh to set up the extract directory
 # Usage: bash extract_viral_reads.sh in a directory containing eg *calmd.bam files
 
+# get current dir containing Wochenende BAM and bam.txt output
+bamDir=$(pwd)
+
+# Refresh Wochenende plot files on calling this script
+cp plots/*plot* extract/
+cp runbatch_metagen_window_filter.sh extract/
+
 taxaToKeep="extract/viruses_2021_02.bed"
 
+
 for i in *calmd.bam; do
+
 	input=$i
 	sec_input=${input%%.bam}
 	
@@ -18,7 +27,7 @@ for i in *calmd.bam; do
 	threads=8
 	queue=short
 
-	# Create new BAM file with reads from references specified in a BED file only
+	echo "INFO: Create new BAM file with reads from references specified in BED file $taxaToKeep only"
 	
 	# SLURM
     #Aligned reads in a region specified by a BED file
@@ -26,11 +35,14 @@ for i in *calmd.bam; do
     srun -c 1 samtools index extract/$sec_input.filt.bam
 
 	# Direct submission, not SLURM
+	samtools view -@ $threads -b -h -L $taxaToKeep -o extract/$sec_input.filt.bam $input 
+    samtools index extract/$sec_input.filt.bam
 
+
+
+	echo "INFO: Extracting data from extracted specified BAMs"
 	window_input_bam=extract/$sec_input.filt.bam	
 	window_input_bam_prefix=${window_input_bam%%.bam}
-
-
 	# Get coverage depth in tiny windows for small ref seqs, eg viruses
 	# 8 threads, Windows 1000, overlap 500, -c minimum coverage. 
 	# SLURM
@@ -40,5 +52,16 @@ for i in *calmd.bam; do
 	#sambamba depth window -t $threads --max-coverage=$covMax --window-size=$window --overlap $overlap -c 0.00001 ${window_input_bam_prefix}.bam > ${window_input_bam_prefix}_cov_window.txt &
 
 
+
 done
 wait
+
+
+echo "INFO: Prepare filtered data for plotting"
+cd extract/
+bash runbatch_metagen_window_filter.sh
+
+
+echo "INFO: Making plots"
+runbatch_wochenende_plot.sh >/dev/null 2>&1
+cd $bamDir
