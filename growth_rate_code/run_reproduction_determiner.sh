e05a1ca0fa69ee806277a8cba5b9fc8b5196b914
poertnes <poertner.sophia@mh-hannover.de>
2021-06-14 11:04:04 +0200

    enhance run scripts

--- growth_rate_code/run_reproduction_determiner.sh
+++ growth_rate_code/run_reproduction_determiner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for csv in $(ls -d *_subsamples);do
-  echo "working on sample "${csv%_subsample}""
-  python3 main.py -p "$csv"
-done
+csv_count=$(ls -d *_subsamples 2>/dev/null | wc -l)
+if [[ $csv_count != 0 ]]
+  then
+  for csv in $(ls -d *_subsamples);do
+    echo "working on sample ${csv%_subsample}"
+    python3 main.py -p "$csv"
+  done
+fi
