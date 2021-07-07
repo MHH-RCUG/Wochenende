2bb1a6c9afc3e7048158f7b798b57568bf240dc1
Colin Davenport <davenport.colin@mh-hannover.de>
2021-07-07 15:45:39 +0200

    links, growth rate updates, WIP

--- growth_rate/run_bed_to_csv.sh
+++ growth_rate/run_bed_to_csv.sh
@@ -8,24 +8,39 @@ echo "Version 0.1"
 #Changelog
 #0.1 - remove bedtools binary and use conda bedtools
 
-# covert bam to bed
+#link bam and bai files found by ls command to current directory
+for i in `ls ../*calmd.bam.bai`
+        do
+        ln -s $i .
+done
+for i in `ls ../*calmd.bam`
+        do
+        ln -s $i .
+done
+
+ls *
+
+# convert *calmd.bam to bed
 bam_count=$(ls -1 *.bam 2>/dev/null | wc -l)
 if [[ $bam_count != 0 ]]
   then
   for bam in *.bam
   do
-    bedtools bamtobed -i "$bam" > "${bam%.bam}".unfiltered.bed
+    #bedtools bamtobed -i "$bam" > "${bam%.bam}".unfiltered.bed
+    bedtools bamtobed -i "$bam" > "${bam%.bam}.unfiltered.bed"
   done
 
   for bed in *.unfiltered.bed
+  
   do
-    #exclude mouse, human, mito
+    #exclude mouse, human, mito chromosomes
     grep -v "^chr" "$bed" | grep -v "^1_1_1" > "${bed%.unfiltered.bed}".bed
-    rm "$bed"  # remove temp file
-    echo "starting bed to csv for file $bed"
+    echo "INFO: Starting bed to csv for file $bed"
     python3 bed_to_pos_csv.py -i "${bed%.unfiltered.bed}".bed -p .
-    echo "completed file $bed"
+    echo "INFO: Completed file $bed"
+
     # cleanup
+    #rm "$bed"  # remove temp file
     if [[ ! -d "${bed%.unfiltered.bed}_subsamples" ]]
       then
       mkdir "${bed%.unfiltered.bed}_subsamples"
@@ -36,5 +51,18 @@ if [[ $bam_count != 0 ]]
         then
         mv "${bed%.unfiltered.bed}"*.csv "${bed%.unfiltered.bed}_subsamples"
     fi
+
+
   done
 fi
+
+
+#unlink bam and bai files
+for i in `ls *calmd.bam.bai`
+        do
+        unlink $i
+done
+for i in `ls *calmd.bam`
+        do
+        unlink $i
+done
