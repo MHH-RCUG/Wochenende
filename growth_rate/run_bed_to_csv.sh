d035bf904d62b5790e268a570056a6035387b79d
poertnes <poertner.sophia@mh-hannover.de>
2021-07-09 14:57:41 +0200

    add runbatch script to start bam to bed convertion parallel via slurm, adapted un_bed_to_csv.sh script

--- growth_rate/run_bed_to_csv.sh
+++ growth_rate/run_bed_to_csv.sh
@@ -1,78 +1,57 @@
 #!/bin/bash
 
-# Prepare Wochenende bam files for the bed to csv script. Makes bed out of Wochenende bam files. Filter out mouse human and mito.
+# Prepare Wochenende bam files for the bed to csv script (input for reproduction determiner)
+# Filter out mouse human and mito chromosomes.
 # Author: Sophia Poertner, 2021
 
-echo "Version 0.11"
+echo "Version 0.12"
 
-#Changelog
-#0.11 -link in bam, bam.txt and bai files, unlink later
-#0.1 - remove bedtools binary and use conda bedtools
+# Changelog
+# 0.12 - get input file from sbatch script for speedup
+# 0.11 - link in bam, bam.txt and bai files, unlink later
+# 0.10 - remove bedtools binary and use conda bedtools
 
-#link bam, bai and bam.txt files found by ls command to current directory
-for i in `ls ../*calmd.bam.txt`
-        do
-        ln -s $i .
-done
-for i in `ls ../*calmd.bam.bai`
-        do
-        ln -s $i .
-done
-for i in `ls ../*calmd.bam`
-        do
-        ln -s $i .
-done
+# check if input exists
+count_bam=`ls -1 ../*calmd.bam 2>/dev/null | wc -l`
+count_bai=`ls -1 ../*calmd.bam.bai 2>/dev/null | wc -l`
+count_bam_txt=`ls -1 ../*calmd.bam.txt 2>/dev/null | wc -l`
 
-ls *
-
-# convert *calmd.bam to bed
-bam_count=$(ls -1 *.bam 2>/dev/null | wc -l)
-if [[ $bam_count != 0 ]]
+if [ $count_bam != 0 ]  && [ $count_bai != 0 ]  && [ $count_bam_txt != 0 ]
   then
-  for bam in *.bam
-  do
-    #bedtools bamtobed -i "$bam" > "${bam%.bam}".unfiltered.bed
-    srun -c 2 bedtools bamtobed -i "$bam" > "${bam%.bam}.unfiltered.bed" &
-    wait
-  done
+  # link bam, bai and bam.txt files found by ls command to current directory
+  ln -s $1 .
+  ln -s ${1%bam}bam.txt .
+  ln -s ${1%bam}bam.bai .
+  ls *
+  bam=${1/..\//}
 
-  for bed in *.unfiltered.bed
-  
-  do
-    #exclude mouse, human, mito chromosomes
-    grep -v "^chr" "$bed" | grep -v "^1_1_1" > "${bed%.unfiltered.bed}".bed
-    echo "INFO: Starting bed to csv for file $bed"
-    python3 bed_to_pos_csv.py -i "${bed%.unfiltered.bed}".bed -p .
-    echo "INFO: Completed file $bed"
+  bedtools bamtobed -i "$bam" > "${bam%.bam}.unfiltered.bed"
 
-    # cleanup
-    #rm "$bed"  # remove temp file
-    if [[ ! -d "${bed%.unfiltered.bed}_subsamples" ]]
+  bed="${bam%.bam}.unfiltered.bed"
+  #exclude mouse, human, mito chromosomes
+  grep -v "^chr" "$bed" | grep -v "^1_1_1" > "${bed%.unfiltered.bed}".bed
+  echo "INFO: Starting bed to csv for file $bed"
+  python3 bed_to_pos_csv.py -i "${bed%.unfiltered.bed}".bed -p .
+  echo "INFO: Completed file $bed"
+
+  # cleanup
+  #rm "$bed"  # remove temp file
+  if [[ ! -d "${bed%.unfiltered.bed}_subsamples" ]]
+    then
+    mkdir "${bed%.unfiltered.bed}_subsamples"
+  fi
+
+  csv_count=$(ls -1 "${bed%.unfiltered.bed}"*.csv 2>/dev/null | wc -l)
+  if [[ $csv_count != 0 ]]
       then
-      mkdir "${bed%.unfiltered.bed}_subsamples"
-    fi
+      mv "${bed%.unfiltered.bed}"*.csv "${bed%.unfiltered.bed}_subsamples"
+  fi
 
-    csv_count=$(ls -1 "${bed%.unfiltered.bed}"*.csv 2>/dev/null | wc -l)
-    if [[ $csv_count != 0 ]]
-        then
-        mv "${bed%.unfiltered.bed}"*.csv "${bed%.unfiltered.bed}_subsamples"
-    fi
-
-
-  done
+  echo "cleanup: unlink bam, bai and bam.txt files"
+  # unlink bam, txt and bai files
+  unlink $bam
+  unlink ${bam%bam}bam.txt
+  unlink ${bam%bam}bam.bai
+else
+  echo "no bam.txt and bai found for input bam. Can't convert to pos.csv"
 fi
-
-
-#unlink bam and bai files
-for i in `ls *calmd.bam.txt`
-        do
-        unlink $i
-done
-for i in `ls *calmd.bam.bai`
-        do
-        unlink $i
-done
-for i in `ls *calmd.bam`
-        do
-        unlink $i
-done
---
+++ growth_rate/runbatch_bed_to_csv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Set variables
+#cpus=1
+## with SLURM srun, default
+#slurm_srun="srun -c $cpus"
+
+# check if input exists
+count_bam=`ls -1 ../*calmd.bam 2>/dev/null | wc -l`
+
+if [ $count_bam != 0 ]
+  then
+  for i in `ls ../*calmd.bam`
+    do
+    sbatch run_bed_to_csv.sh $i
+  done
+else
+  echo "No bam file found. Can't convert to pos.csv"
+fi
