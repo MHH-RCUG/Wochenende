d8799076fada2bf4cdae6513cb0f84db9430c892
Colin Davenport <davenport.colin@mh-hannover.de>
2021-11-05 17:00:36 +0100

    Refactor long running methods to end

--- wochenende_postprocess.sh
+++ wochenende_postprocess.sh
@@ -114,7 +114,7 @@ fi
 ## Set command line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -a) runAll="1";  ;;
+ #       -a) runAll="1";  ;;
         -r) runReporting="1";  ;;
         -s) runRaspir="1";  ;;
         -p) runPlotting="1";  ;;
@@ -124,7 +124,7 @@ while [[ "$#" -gt 0 ]]; do
     esac
     shift
 done
-echo "INFO: Arguments Run all stages (-a)   : $runAll"
+#echo "INFO: Arguments Run all stages (-a)   : $runAll"
 echo "INFO: Arguments Run reporting (-r)    : $runReporting"
 echo "INFO: Arguments Run haybaler (-h)     : $runHaybaler"
 echo "INFO: Arguments Run Raspir (-s)       : $runRaspir"
@@ -138,66 +138,162 @@ echo "INFO: Current directory" $bamDir >>$output_log 2>&1
 sleep 3
 
 
-echo "INFO: Started Sambamba depth"
 bash runbatch_metagen_awk_filter.sh
 wait
-if [[ $1 == "--no-plots" ]] 
+
+
+# Run reporting 
+if [[ $runReporting == "1" ]] 
+    echo "INFO: Started Wochenende reporting"
+    echo "INFO: Started Wochenende reporting" >>$output_log 2>&1
+    cd $bamDir
+    cd reporting
+    cp ../*.bam.txt .
+    bash runbatch_Wochenende_reporting.sh >>$output_log 2>&1
+    wait
+    echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
+    sleep $sleeptimer
+    echo "INFO: Completed Wochenende reporting"
+fi
+
+# Run haybaler
+if [[ $runHaybaler == "1" ]] 
+    echo "INFO: Start Haybaler"
+    echo "INFO: Start Haybaler" >>$output_log 2>&1
+    if [[ ! -d "haybaler" ]]
+        then
+        mkdir haybaler
+    fi
+    # count files and only copy files that exist to avoid missing files errors
+    count_mq20=`ls -1 *mq20.bam*us*.csv 2>/dev/null | wc -l`
+    count_mq30=`ls -1 *mq30.bam*us*.csv 2>/dev/null | wc -l`
+    count_dup=`ls -1 *dup.bam*us*.csv 2>/dev/null | wc -l`
+    count=`ls -1 *.bam*us*.csv 2>/dev/null | wc -l`
+    if [[ $count_mq30 != 0 ]]
+        then
+        cp *mq30.bam*us*.csv haybaler
+    elif [[ $count_mq20 != 0 ]]
+        then
+        cp *mq20.bam*us*.csv haybaler
+    elif [[ $count_dup != 0 ]]
+        then
+        cp *dup.bam*us*.csv haybaler
+    elif [[ $count != 0 ]]
+        then
+        cp *.bam*us*.csv haybaler
+    else
+        echo "WARNING: No bam*us*.csv found to process for haybaler"
+        echo "INFO: Attempting to find and copy bam*us*.csv to haybaler"
+        cp txt/*.bam*us*.csv haybaler
+        cp csv/*.bam*us*.csv haybaler
+    fi
+    cd haybaler
+    conda activate $HAYBALER_CONDA_ENV_NAME
+    cp $haybaler_dir/*.sh .
+    cp $haybaler_dir/*.py .
+    cp $haybaler_dir/*.R .
+    bash run_haybaler.sh $haybaler_dir >>$output_log 2>&1
+    wait
+    cp $haybaler_dir/runbatch_heatmaps.sh haybaler_output/ && cp $haybaler_dir/*.R haybaler_output/
+    cp $haybaler_dir/*tax* haybaler_output/
+    cp $haybaler_dir/*tree* haybaler_output/
+
+    echo "INFO: Attempting to filter results and create heatmaps. Requires R installation." 
+    cd haybaler_output
+    bash runbatch_heatmaps.sh >>$output_log 2>&1
+    echo "INFO: Attempting to add taxonomy. Requires pytaxonkit." 
+    bash run_haybaler_tax.sh >>$output_log 2>&1
+    echo "INFO: Attempting create heat-trees. Requires R installation and packages: packages = c("metacoder", "taxa", "dplyr", "tibble", "ggplot2")." 
+    bash run_heattrees.sh >>$output_log 2>&1
+    cd ..
+    cd ..
+    echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
+    sleep $sleeptimer
+
+    echo "INFO: Start csv to xlsx conversion"
+    bash runbatch_csv_to_xlsx.sh >>$output_log 2>&1
+    wait
+    echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
+    sleep $sleeptimer
+    echo "INFO: Completed Haybaler"
+fi
+
+
+
+if [[ $runReporting == "1" ]] 
+    echo "INFO: Start cleanup reporting"
+    cd $bamDir
+    cd reporting
+    # create backup, move folders from previous reporting run to a directory (if the txt directory exists already)
+    mkdir reporting_$rand_number
+    if [[ -d "txt" ]]
+        then
+        mv txt/ csv/ xlsx/ reporting_$rand_number
+    fi
+
+    # make and fill current folders from this run
+    mkdir txt csv xlsx
+
+    # cleanup .txt, .csv and .xlsx files if they exist in directory
+    count=`ls -1 *.txt 2>/dev/null | wc -l`
+    if [[ $count != 0 ]]
+        then 
+        mv *.txt txt
+    fi 
+    count=`ls -1 *.csv 2>/dev/null | wc -l`
+    if [[ $count != 0 ]]
+        then 
+        mv *.csv csv
+    fi 
+    count=`ls -1 *.xlsx 2>/dev/null | wc -l`
+    if [[ $count != 0 ]]
+        then 
+        mv *.xlsx xlsx
+    fi 
+
+    cd $bamDir
+    echo "INFO: Completed cleanup reporting"
+fi
+
+if [[ $runPlotting == "1" ]] 
     then
-    echo "INFO: Found --no-plots argument: Skipping runbatch_sambamba_depth.sh"
-else
+    echo "INFO: Started Sambamba depth"
     bash runbatch_sambamba_depth.sh >>/dev/null 2>&1
     wait
     echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
     sleep $sleeptimer
     bash runbatch_metagen_window_filter.sh >>$output_log 2>&1
     wait
+    echo "INFO: Completed Sambamba depth and filtering"
 fi
-echo "INFO: Completed Sambamba depth and filtering"
-
-# raspir
-echo "INFO: Run raspir by Marie-Madlen Pust"
-echo "INFO: Run raspir by Marie-Madlen Pust"  >>$output_log 2>&1
-cd $bamDir/raspir
-echo "INFO: link BAM files in"  >>$output_log 2>&1
-bash batch_create_links.sh  >>$output_log 2>&1
-echo "INFO: Start preparing the files for raspir"  >>$output_log 2>&1
-bash run_SLURM_file_prep.sh  >>$output_log 2>&1
-echo "INFO: Run raspir"  >>$output_log 2>&1
-bash run_raspir_SLURM.sh  >>$output_log 2>&1
-echo "INFO: Remove soft linked BAM files"  >>$output_log 2>&1
-batch_remove_links.sh  >>$output_log 2>&1
-cd $bamDir
-echo "INFO: Raspir module completed"
 
 
 # Growth rate
-echo "INFO: Started bacterial growth rate analysis"
-echo "INFO: Started bacterial growth rate analysis" >>$output_log 2>&1
-cd $bamDir
-cd growth_rate/
-echo "INFO: Cleanup original results" >>$output_log 2>&1
-rm -rf fit_results >>/dev/null 2>&1
-rm -rf *_subsamples >>/dev/null 2>&1
-bash runbatch_bed_to_csv.sh  >>$output_log 2>&1 
-wait
-echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
-sleep $sleeptimer
+if [[ $runGrowth == "1" ]] 
+    echo "INFO: Started bacterial growth rate analysis"
+    echo "INFO: Started bacterial growth rate analysis" >>$output_log 2>&1
+    cd $bamDir
+    cd growth_rate/
+    echo "INFO: Cleanup original results" >>$output_log 2>&1
+    rm -rf fit_results >>/dev/null 2>&1
+    rm -rf *_subsamples >>/dev/null 2>&1
+    bash runbatch_bed_to_csv.sh  >>$output_log 2>&1 
+    wait
+    echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
+    sleep $sleeptimer
 
-bash run_reproduction_determiner.sh  >>$output_log 2>&1
-wait
-echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
-sleep $sleeptimer
-echo "INFO: Files produced by growth rate"   >>$output_log 2>&1
-ls "growth_rate/fit_results/output/*" >>$output_log 2>&1
-cd $bamDir
-echo "INFO: Completed bacterial growth rate analysis, see growth_rate/fit_results/output for results"
-
+    bash run_reproduction_determiner.sh  >>$output_log 2>&1
+    wait
+    echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
+    sleep $sleeptimer
+    echo "INFO: Files produced by growth rate"   >>$output_log 2>&1
+    ls "growth_rate/fit_results/output/*" >>$output_log 2>&1
+    cd $bamDir
+    echo "INFO: Completed bacterial growth rate analysis, see growth_rate/fit_results/output for results"
+fi
 
 # Plots
-if [[ $1 == "--no-plots" ]] 
-then
-    echo "INFO: Found --no-plots argument: Plot mode disabled"
-else
+if [[ $runPlotting == "1" ]] 
     echo "INFO: Started Wochenende plot"
     echo "INFO: Started Wochenende plot" >>$output_log 2>&1
     cd $bamDir
@@ -211,126 +307,33 @@ else
     sleep $sleeptimer
     cd $bamDir
     echo "INFO: Completed Wochenende plot"
+
+    echo "INFO:  - Extracting selected human viral pathogen reads"
+    echo "INFO:  - Extracting selected human viral pathogen reads" >>$output_log 2>&1
+    cd $bamDir
+    bash extract_viral_reads.sh >>$output_log 2>&1
+    wait
+    echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
+    sleep $sleeptimer
 fi
 
-echo "INFO:  - Extracting selected human viral pathogen reads"
-echo "INFO:  - Extracting selected human viral pathogen reads" >>$output_log 2>&1
-cd $bamDir
-bash extract_viral_reads.sh >>$output_log 2>&1
-wait
-echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
-sleep $sleeptimer
-
-# Run reporting 
-echo "INFO: Started Wochenende reporting"
-echo "INFO: Started Wochenende reporting" >>$output_log 2>&1
-cd $bamDir
-cd reporting
-cp ../*.bam.txt .
-bash runbatch_Wochenende_reporting.sh >>$output_log 2>&1
-wait
-echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
-sleep $sleeptimer
-
-echo "INFO: Completed Wochenende reporting"
-
-
-# Run haybaler
-echo "INFO: Start Haybaler"
-echo "INFO: Start Haybaler" >>$output_log 2>&1
-if [[ ! -d "haybaler" ]]
-    then
-    mkdir haybaler
-fi
-# count files and only copy files that exist to avoid missing files errors
-count_mq20=`ls -1 *mq20.bam*us*.csv 2>/dev/null | wc -l`
-count_mq30=`ls -1 *mq30.bam*us*.csv 2>/dev/null | wc -l`
-count_dup=`ls -1 *dup.bam*us*.csv 2>/dev/null | wc -l`
-count=`ls -1 *.bam*us*.csv 2>/dev/null | wc -l`
-if [[ $count_mq30 != 0 ]]
-    then
-    cp *mq30.bam*us*.csv haybaler
-elif [[ $count_mq20 != 0 ]]
-    then
-    cp *mq20.bam*us*.csv haybaler
-elif [[ $count_dup != 0 ]]
-    then
-    cp *dup.bam*us*.csv haybaler
-elif [[ $count != 0 ]]
-    then
-    cp *.bam*us*.csv haybaler
-else
-    echo "WARNING: No bam*us*.csv found to process for haybaler"
-    echo "INFO: Attempting to find and copy bam*us*.csv to haybaler"
-    cp txt/*.bam*us*.csv haybaler
-    cp csv/*.bam*us*.csv haybaler
-fi
-cd haybaler
-conda activate $HAYBALER_CONDA_ENV_NAME
-cp $haybaler_dir/*.sh .
-cp $haybaler_dir/*.py .
-cp $haybaler_dir/*.R .
-bash run_haybaler.sh $haybaler_dir >>$output_log 2>&1
-wait
-cp $haybaler_dir/runbatch_heatmaps.sh haybaler_output/ && cp $haybaler_dir/*.R haybaler_output/
-cp $haybaler_dir/*tax* haybaler_output/
-cp $haybaler_dir/*tree* haybaler_output/
-
-echo "INFO: Attempting to filter results and create heatmaps. Requires R installation." 
-cd haybaler_output
-bash runbatch_heatmaps.sh >>$output_log 2>&1
-echo "INFO: Attempting to add taxonomy. Requires pytaxonkit." 
-bash run_haybaler_tax.sh >>$output_log 2>&1
-echo "INFO: Attempting create heat-trees. Requires R installation and packages: packages = c("metacoder", "taxa", "dplyr", "tibble", "ggplot2")." 
-bash run_heattrees.sh >>$output_log 2>&1
-cd ..
-cd ..
-echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
-sleep $sleeptimer
-
-echo "INFO: Start csv to xlsx conversion"
-bash runbatch_csv_to_xlsx.sh >>$output_log 2>&1
-wait
-echo "INFO: Sleeping for "$sleeptimer "to allow writes to complete."
-sleep $sleeptimer
-echo "INFO: Completed Haybaler"
-
-
-
-
-
-echo "INFO: Start cleanup reporting"
-cd $bamDir
-cd reporting
-# create backup, move folders from previous reporting run to a directory (if the txt directory exists already)
-mkdir reporting_$rand_number
-if [[ -d "txt" ]]
-    then
-    mv txt/ csv/ xlsx/ reporting_$rand_number
+# raspir
+if [[ $runRaspir == "1" ]] 
+    echo "INFO: Run raspir by Marie-Madlen Pust"
+    echo "INFO: Run raspir by Marie-Madlen Pust"  >>$output_log 2>&1
+    cd $bamDir/raspir
+    echo "INFO: link BAM files in"  >>$output_log 2>&1
+    bash batch_create_links.sh  >>$output_log 2>&1
+    echo "INFO: Start preparing the files for raspir"  >>$output_log 2>&1
+    bash run_SLURM_file_prep.sh  >>$output_log 2>&1
+    echo "INFO: Run raspir"  >>$output_log 2>&1
+    bash run_raspir_SLURM.sh  >>$output_log 2>&1
+    echo "INFO: Remove soft linked BAM files"  >>$output_log 2>&1
+    batch_remove_links.sh  >>$output_log 2>&1
+    cd $bamDir
+    echo "INFO: Raspir module completed"
 fi
 
-# make and fill current folders from this run
-mkdir txt csv xlsx
-
-# cleanup .txt, .csv and .xlsx files if they exist in directory
-count=`ls -1 *.txt 2>/dev/null | wc -l`
-if [[ $count != 0 ]]
-    then 
-    mv *.txt txt
-fi 
-count=`ls -1 *.csv 2>/dev/null | wc -l`
-if [[ $count != 0 ]]
-    then 
-    mv *.csv csv
-fi 
-count=`ls -1 *.xlsx 2>/dev/null | wc -l`
-if [[ $count != 0 ]]
-    then 
-    mv *.xlsx xlsx
-fi 
-
-cd $bamDir
-echo "INFO: Completed cleanup reporting"
 
 echo "INFO: Remember to check the output log for errors at: " $output_log
 echo "INFO: ########### Completed Wochenende_postprocess #############"
