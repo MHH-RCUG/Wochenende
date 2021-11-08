c266e430fe0b4d23d66235056aa9828ab8406765
Colin Davenport <davenport.colin@mh-hannover.de>
2021-11-08 16:46:05 +0100

    Reporting conda and sbatch config updates

--- plots/runbatch_wochenende_plot.sh
+++ plots/runbatch_wochenende_plot.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-# Colin Davenport, Jan 2020 - Jul 2021
+# Colin Davenport, Jan 2020 - Nov 2021
 # Run Wochenende plotting as batch, add SLURM
 
-# Set variables
-cpus=1
-#with SLURM srun, default
-slurm_srun="srun -c $cpus"
+
+# Setup SLURM using data parsed from config.yaml
+source $WOCHENENDE_DIR/scripts/parse_yaml.sh
+eval $(parse_yaml $WOCHENENDE_DIR/config.yaml)
+# Setup job scheduler
+# use SLURM job scheduler (yes, no)
+if [[ "${USE_CUSTOM_SCHED}" == "yes" ]]; then
+    #echo USE_CUSTOM_SCHED set"
+    scheduler=$CUSTOM_SCHED_CUSTOM_PARAMS_SINGLECORE
+fi
+if [[ "${USE_SLURM}" == "yes" ]]; then
+    #echo USE_SLURM set"
+    scheduler=$SLURM_CUSTOM_PARAMS_SINGLECORE
+fi
 
 # Save output log in directory containing bams and preprocess script
 output_log="plot_"$(date +%s)".log"
 echo "INFO: output_log: " $output_log
 
-# mv existing images to a backup directory
+# move existing images to a backup directory
 images_backup="images_old_"$(date +%s)
 mv images $images_backup
 
 for i in $(ls *cov_window.txt.filt.csv)
         do
 		
-		# filter out human reads and sort by total reads per window
-		#python wochenende_plot.py "$i" >> $output_log
-		# slurm version
-		$slurm_srun python wochenende_plot.py "$i" >> $output_log &
+		# plot the prepared filtered csv files
+		$scheduler python wochenende_plot.py "$i" >> $output_log &
 
 done
 wait
--- reporting/run_Wochenende_reporting_SLURM.sh
+++ reporting/run_Wochenende_reporting_SLURM.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 ## Supply the Wochenende bam.txt input as arg1, bash run_Wochenende_reporting_SLURM.sh in.bam.txt
 
-# set partition
-#SBATCH -p normal
-
-# set run on x MB node only
-#SBATCH --mem 40000
-
-# set run on bigmem node only
-#SBATCH --cpus-per-task 4
-
-# share node
-#SBATCH --share
-
-# set max wallclock time
-# SBATCH --time=4:00:00
-
-# set name of job
-#SBATCH --job-name=Wo_report
-
-# mail alert at start, end and abortion of execution
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=<mailaddress>
 
 echo "Input bam: " $1
 bamtxt=$1
 
-# Source miniconda3
-. /mnt/ngsnfs/tools/miniconda3/etc/profile.d/conda.sh
+# Setup SLURM using data parsed from config.yaml
+source $WOCHENENDE_DIR/scripts/parse_yaml.sh
+eval $(parse_yaml $WOCHENENDE_DIR/config.yaml)
+# Setup job scheduler
+# use SLURM job scheduler (yes, no)
+if [[ "${USE_CUSTOM_SCHED}" == "yes" ]]; then
+    #echo USE_CUSTOM_SCHED set"
+    scheduler=$CUSTOM_SCHED_CUSTOM_PARAMS_SINGLECORE
+fi
+if [[ "${USE_SLURM}" == "yes" ]]; then
+    #echo USE_SLURM set"
+    scheduler=$SLURM_CUSTOM_PARAMS_SINGLECORE
+fi
 
-# Activate env on cluster node
-conda activate wochenende
+# Set and activate existing conda env
+. $CONDA_SH_PATH
+conda activate $WOCHENENDE_CONDA_ENV_NAME
 
 
 # read reference path from ref.tmp file in the reporting (current) directory 
@@ -38,10 +29,5 @@ ref=$(<ref.tmp)
 echo "$ref"
 
 
-
 # Run script
-srun python3 basic_reporting.py --input_file $bamtxt --reference $ref --sequencer illumina --output_name $bamtxt
-
-#conda activate wochenende
-#python3 basic_reporting.py --input_file tmp_R1.ndp.lc.trm.s.mq30.01mm.dup.bam.txt --refseq_file /mnt/ngsnfs/seqres/metagenref/2016_06_PPKC_metagenome_test_1p_spec_change_cln.fa --sequencer illumina --sample_name test
-
+$scheduler python3 basic_reporting.py --input_file $bamtxt --reference $ref --sequencer illumina --output_name $bamtxt
