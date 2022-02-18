44ddd646ee67a74334364d5f0d01517d52be65a6
colindaven <colindaven@hotmail.com>
2022-02-18 14:39:59 +0100

    correct names after testing

--- growth_rate/main.py
+++ growth_rate/main.py
@@ -67,7 +67,7 @@ def evaluate_all_data(sample_folder_path, experiment_name, save_plots=True):
                             'Initial_Bins': initial_bins, 'Used_Bins': used_bins,
                             'Fit_Err': bins_fit_error, 'Error_Codes': error_codes})
     # change filename from generic results_summary.csv to include sample name, also one level up
-    filename = save_path / ".." / (png_name + '_results.csv')
+    filename = save_path / ".." / (sample_folder_path.name + '_results.csv')
     #filename = save_path / 'results_summary.csv'
     # save results to a single csv file
     df.to_csv(filename, sep=',', index=False, header=True, float_format='%.2f')
